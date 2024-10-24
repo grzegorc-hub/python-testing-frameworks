29e89eb40e55ec6fc703cacb26a69b13edfbf702
Maciej <grzegorc1@gmail.com>
2024-10-24 20:46:41 +0200

    updated tests

--- tests/pytest/test_cat_detector_pytest.py
+++ tests/pytest/test_cat_detector_pytest.py
@@ -1,15 +1,14 @@
 """
-
+Test for CatDetector class. It runs CatDetector on test image and checks if detection result is correct, 
+that is e.g number of detected cats should be equal to actual number of cats on the image.
 """
 import pytest
 from demo_app.cat_detector import CatDetector
-from tests.util import extractYoloResult
 
 TEST_IMAGE_FILENAME = 'demo_app/Girl_and_cat.jpg' 
 EXPECTED_NUMBER_OF_CATS = 1
 EXPECTED_NUMBER_OF_PEOPLE = 1
 EXPECTED_NUMBER_OF_OBJECTS = EXPECTED_NUMBER_OF_CATS + EXPECTED_NUMBER_OF_PEOPLE
-EXPECTED_NUMBER_OF_OBJECT_TYPES = 2
 MIN_CONFIDENCE = 0.5
 
 
@@ -19,12 +18,10 @@ def cat_detector():
 
 
 def test_basic_cat_detection(cat_detector):
-    result_data = cat_detector.run(TEST_IMAGE_FILENAME)
-    boxes, names, clss, confs = extractYoloResult(result_data)
-    cat_count = clss.count(15.0)
-    people_count = clss.count(0.0)
-    assert len(boxes) == EXPECTED_NUMBER_OF_OBJECTS, "Incorrect number of bounding boxes!"
+    cat_count, people_count, boxes, _, confs, _ = cat_detector.run(TEST_IMAGE_FILENAME)
+
     assert cat_count == EXPECTED_NUMBER_OF_CATS, "Incorrect nubmer of detected cats!"
     assert people_count == EXPECTED_NUMBER_OF_PEOPLE, "Incorrect nubmer of detected people!"
+    assert len(boxes) == EXPECTED_NUMBER_OF_OBJECTS, "Incorrect number of bounding boxes!"
     for i in range(EXPECTED_NUMBER_OF_OBJECTS):
         assert confs[i] >= MIN_CONFIDENCE, f"Object no. {i} detected with too low confidence"
--- tests/unittest/test_cat_detector.py
+++ tests/unittest/test_cat_detector.py
@@ -1,11 +1,9 @@
 """
-Module test for CatDetector class.
-It runs CatDetector on predefined test image and then it checks if detection output is as expected, that is e.g
-number of detected cats by CatDetector should be equal to actual number of cats on the image
+Test for CatDetector class. It runs CatDetector on test image and checks if detection result is correct, 
+that is e.g number of detected cats should be equal to actual number of cats on the image.
 """
 import unittest
 from demo_app.cat_detector import CatDetector
-from tests.util import extractYoloResult
 
 
 class TestCatDetector(unittest.TestCase):
@@ -13,20 +11,17 @@ class TestCatDetector(unittest.TestCase):
     EXPECTED_NUMBER_OF_CATS = 1
     EXPECTED_NUMBER_OF_PEOPLE = 1
     EXPECTED_NUMBER_OF_OBJECTS = EXPECTED_NUMBER_OF_CATS + EXPECTED_NUMBER_OF_PEOPLE
-    EXPECTED_NUMBER_OF_OBJECT_TYPES = 2
     MIN_CONFIDENCE = 0.5
 
     def setUp(self):
         self.cat_detector = CatDetector()
 
     def test_basic_yolo_detection(self):
-        result_data = self.cat_detector.run(self.TEST_IMAGE_FILENAME)
-        boxes, names, clss, confs = extractYoloResult(result_data)
-        cat_count = clss.count(15.0)
-        people_count = clss.count(0.0)
-        self.assertEqual(len(boxes), self.EXPECTED_NUMBER_OF_OBJECTS, "Incorrect number of bounding boxes!")
+        cat_count, people_count, boxes, _, confs, _ = self.cat_detector.run(self.TEST_IMAGE_FILENAME)
+        
         self.assertEqual(cat_count, self.EXPECTED_NUMBER_OF_CATS, "Incorrect nubmer of detected cats!")
         self.assertEqual(people_count, self.EXPECTED_NUMBER_OF_PEOPLE, "Incorrect nubmer of detected people!")
+        self.assertEqual(len(boxes), self.EXPECTED_NUMBER_OF_OBJECTS, "Incorrect number of bounding boxes!")
         for i in range(self.EXPECTED_NUMBER_OF_OBJECTS):
             self.assertGreaterEqual(confs[i], self.MIN_CONFIDENCE, f"Object no. {i} detected with too low confidence")
 
